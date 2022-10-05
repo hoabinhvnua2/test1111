99d6ca993ec1d7a61649cbcfd419d4512391ad18
binhth <binh@tokenize.exchange>
2022-10-05 14:00:33 +0700

    đsds

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Component/Layout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}></Route>
+    </Routes>
   );
 }
 
